4c873d1198ddb88e63196eaa93fd889f93e9b08c
Aluno <Aluno@10.85.198.42>
2024-06-13 14:24:18 -0300

    java project InfiniteLoop

---
+++ src/InfiniteLoop.java
@@ -0,0 +1,9 @@
+public class InfiniteLoop {
+    public static void main(String[] args) {
+        
+        // O loop for vai rodar 5 vezes.
+        for(int i = 0; i < 5; i++){
+            System.out.println("Ola");
+        }
+    }
+}
